f510439c70b14002f48276ccafc21abbde068aac
Omar Balagadashov <mromar.work@gmail.com>
2024-02-28 15:03:56 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
-      });
-    }
-    return config;
-  },
+  output: "export"
 };
 
 module.exports = nextConfig;
