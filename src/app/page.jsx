b1b32a48ef958829c7770335346e1fb0be7665cd
Omar Balagadashov <mromar.work@gmail.com>
2024-03-02 10:19:19 +0000

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import logo from "../../public/fromfolio.svg";
+import logo from "../../public/formfolio.svg";
 import Form from "@/components/Form";
 import Notification from "@/components/Notification";
 
